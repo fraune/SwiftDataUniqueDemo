d5c6e6c72ca79e009a7f45d05707a3cb6c93657b
Brandon Fraune <brandon.fraune@gmail.com>
2024-04-30 21:07:16 -0500

    new project

---
+++ SwiftDataUniqueDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataUniqueDemo
+//
+//  Created by Brandon Fraune on 4/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataUniqueDemo/SwiftDataUniqueDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataUniqueDemoApp.swift
+//  SwiftDataUniqueDemo
+//
+//  Created by Brandon Fraune on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataUniqueDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
